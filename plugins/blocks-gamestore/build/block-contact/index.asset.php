c87f1cb4954135dae68f625963c17e3331f1a238
podyankee <podyankee@gmail.com>
2025-01-17 06:26:58 +0300

    ci-cd test

---
+++ plugins/blocks-gamestore/build/block-contact/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'bad623d00386e8853c93');
---
+++ plugins/core-gamestore/core-gamestore.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+* Plugin Name: Core Gamestore
+* Plugin URI: #
+* Description: Core Code for Gamestore
+* Author: Kostyantin
+* Version: 1.0
+* Author URI: designerccc@yandex.ru
+* License: GPLv2 or later
+* Text Domain: core-gamestore
+* Domain Path: /languages
+*/
+
+define('GAMESTORE_PLUGIN_URL', plugin_dir_url(__FILE__));
+define('GAMESTORE_PLUGIN_PATH', plugin_dir_path(__FILE__));
